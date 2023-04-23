4fe5bdf46ede41e017c914d463cc355b91a53eca
Brooklyn Gibbs <brogibbs@iu.edu>
2023-04-22 20:55:57 -0500

    wrkout cards

--- src/components/WorkoutItem.js
+++ src/components/WorkoutItem.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import "../styles/WorkoutPrograms.css";
+import arms from "../assets/workout/arms.jpg";
+import arms2 from "../assets/workout/armsTwo.jpg";
+import arms3 from "../assets/workout/armsThree.jpg";
+import back from "../assets/workout/back.jpg";
+import back2 from "../assets/workout/backTwo.jpg";
+import back3 from "../assets/workout/backThree.jpg";
+import legs from "../assets/workout/legs.jpg";
+import legs2 from "../assets/workout/legsTwo.jpg";
+import legs3 from "../assets/workout/legsThree.jpg";
+import wk1 from "../assets/workout/wk1.jpg";
+import wk3 from "../assets/workout/wk2.jpg";
+import wk2 from "../assets/workout/wk3.jpg";
 
-function WorkoutItem({ image, name, difficulty }) {
+const workoutImages = [arms, arms2, arms3, back, back2, back3, legs, legs2, legs3, wk1, wk2, wk3];
+
+function WorkoutItem({name, difficulty, views, rating, duration, contentURL}) {
+  
+  const handleClick = () => {
+    window.location.href = contentURL;
+  };
+
+  const imageIndex = Math.floor(Math.random() * workoutImages.length);
+  const imagePath = workoutImages[imageIndex];
+
   return (
-    <div className="menuItem">
-      <div style={{ backgroundImage: `url(${image})` }}> </div>
+    <div className="menuItem" onClick={handleClick}>
+      <div style={{ backgroundImage: `url(${imagePath})`, height: '100%'}}> </div>
       <h1> {name} </h1>
-      <p> {difficulty} </p>
+      <p> Level: {difficulty} </p>
+      <p> {views} views | {rating} stars | {duration} minutes</p>
     </div>
   );
 }
--- src/pages/WorkoutPrograms.js
+++ src/pages/WorkoutPrograms.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { WorkoutList } from "../helpers/WorkoutList";
 import WorkoutItem from "../components/WorkoutItem";
-import "../styles/Workout.css";
+import "../styles/WorkoutPrograms.css";
+import axios from "axios";
+import { useRef, useState, useEffect } from "react";
 
 function WorkoutPrograms() {
+  const [workouts, setWorkouts] = useState([]);
+  const CONTENT_URL = "http://gojim-backend.eastasia.cloudapp.azure.com/content";
+  const token = localStorage.getItem('token');
+  
+  useEffect(() => {
+    axios.get(CONTENT_URL, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      setWorkouts(response.data.result);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
+
   return (
-    <div className="menu">
-      <h1 className="menuTitle">Workouts</h1>
-      <div className="menuList">
-        {WorkoutList.map((workoutItem, key) => {
-          return (
-            <WorkoutItem
-              key={key}
-              image={workoutItem.image}
-              name={workoutItem.name}
-              difficulty={workoutItem.difficulty}
-            />
-          );
-        })}
-      </div>
+    <div className="workoutsContainer">
+      {workouts.map((workout, index) => (
+        <WorkoutItem
+          key={index}
+          name={workout.title}
+          difficulty={workout.difficulty_level}
+          views={workout.views}
+          rating={workout.rating}
+          duration={workout.duration}
+          contentURL={workout.content_url}
+        />
+      ))}
     </div>
   );
 }
